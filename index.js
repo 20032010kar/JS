cf13f5d4824a68561e26477661c1302e59f39c3a
20032010kar <robloxkarina94@gmail.com>
2026-05-31 17:09:46 +0300

    add files

---
+++ index.js
@@ -0,0 +1,314 @@
+"use strict"
+
+// message = "Hello world"
+// console.log(message)
+
+// Діалоги
+// Просто виводить діалог з повідомленням
+// alert("You just  open this page. Welcome, pal");
+
+// Діалог в якому у користувача запитують якесь значення, завжди повертає string
+/*const result = prompt("Hello what your name?");
+alert(`Hello, ${result}`);*/
+
+/*1*/ 
+let age = prompt("How old are you?");
+if(age <=12){
+    alert("You are child");
+}
+if(age > 12 && age < 18){
+    alert("You are teenager");
+}
+if(age >= 18 && age <= 60){
+    alert("You are adult");
+}
+if(age > 60){
+    alert("You are adult");
+}
+
+/*2*/ 
+let num = prompt("Enter a number 0-9");
+switch(num){
+    case "1":
+        alert("!");
+        break;
+    case "2":
+        alert("@");
+        break;
+    case "3":
+        alert("#");
+        break;
+    case "4":
+        alert("$");
+        break;
+    case "5":
+        alert("%");
+        break;
+    case "6":
+        alert("^");
+        break;
+    case "7":
+        alert("&");
+        break;
+    case "8":
+        alert("*");
+         break;
+         case "9" :
+            alert("(")
+            break;
+        case "0" :
+            alert("Bye");
+            break;
+        default:
+            alert("Wrong number");
+}
+
+
+/*3*/ 
+
+let number = prompt("Enter a three-digit number");
+
+if (number[0] === number[1] && number[1] === number[2]) {
+    alert("This number has 3 same digits");
+} 
+else if (number[0] === number[1] || number[0] === number[2] || number[1] === number[2]) {
+    alert("This number has 2 same digits");
+} 
+else {
+    alert("This number has no same digits");
+}
+
+
+/*4*/ 
+let  year = prompt("Enter a year");
+if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+    alert("This year is a leap year");
+}
+else{
+    alert("This year is not a leap year");
+}
+
+/*5*/ 
+let valute = prompt("Enter a coun tof USD");
+let convert = prompt("Enter a valute to convert(EUR,UAN,AZN): ");
+
+const EUR = 1.17;
+const UAN = 0.15;
+const AZN = 0.59;
+
+let result;
+
+switch(convert){
+    case "EUR":
+        result = valute * EUR;
+        break;
+    case "UAN":
+        result = valute * UAN;
+        break;
+    case "AZN":
+        result = valute * AZN;
+        break;  
+    default:
+        alert("Wrong valute");
+        result = null;
+}
+
+if(result !== null){
+    alert(`You have ${result} ${convert}`);
+}
+
+/*6*/ 
+let sum = prompt("Enter a sum of buying");
+let discount = 0;
+
+if(sum>= 200 && sum <300){
+    alert("You have 3% discount");
+    discount = sum * 0.03;
+}
+if(sum >= 300 && sum < 500){
+    alert("You have 5% discount");
+    discount = sum * 0.05;
+}
+if(sum >= 500){
+    alert("You have 7% discount");
+    discount = sum * 0.07;
+}
+let resultSum = sum - discount;
+alert(`Your sum with discount is ${resultSum}`);
+
+
+/*7*/ 
+let wantPlay = prompt("Hi! I have for you 3 questions. Do you want to play with me? (Type 'yes' to play)");
+
+if (wantPlay === "yes") {
+    let score = 0; 
+    let correctAnswersCount = 0;
+
+    let answer = prompt(`Which human body organ has the greatest mass?
+        1)Skin
+        2)Liver
+        3)Lungs`);
+    
+    let answer2 = prompt(`Which country has the largest land area in the world?
+        1)Russia
+        2)Canada
+        3)China`);
+    
+    let answer3 = prompt(`Which of these is an object in JavaScript?
+        1) { "name": "Gemini" }
+        2) [1, 2, 3] 
+        3)"Hello" `);
+
+    switch (answer) {
+        case "1":
+            
+            score += 2;
+            break;
+        default:
+            alert("Wrong answer!");
+    }
+
+    switch (answer2) {
+        case "1":
+            
+            score += 2;
+            break;
+        default:
+            alert("Wrong answer!");
+    }
+
+    switch (answer3) {
+        case "1":
+            
+            score += 2;
+            break;
+        default:
+            alert("Wrong answer!");
+    }
+    alert(`Game over! You answered correctly to ${correctAnswersCount} questions. Your total score is: ${score} points.`);
+} else {
+    alert("Bye! See you later!");
+}
+
+/*7*/ 
+let start = prompt("Hi! I have a quiz for you. Do you want to play?");
+alert("Ok, let's start! Good Luck.");
+alert("------ ANIMALS QUIZ ------");
+
+let score = 0;
+let errors = "";
+
+let q1 = prompt("1. Which animal is the largest land mammal? \nA) African elephant; B) Giraffe; C) Hippopotamus; D) White rhinoceros.");
+if (q1 == "A") { score++; } 
+else { errors += "1. Largest land mammal\n"; }
+
+let q2 = prompt("2. Which bird can fly backwards? \nA) Swallow; B) Hummingbird; C) Sparrow; D) Owl.");
+if (q2 == "B") { score++; } 
+else { errors += "2. Bird flying backwards\n"; }
+
+let q3 = prompt("3. Which animal is the fastest on land? \nA) Lion; B) Wildebeest; C) Cheetah; D) Zebra.");
+if (q3 == "C") { score++; } 
+else { errors += "3. Fastest animal\n"; }
+
+let q4 = prompt("4. How many chambers are in the heart of most mammals? \nA) 2; B) 3; C) 4; D) 5.");
+if (q4 == "C") { score++; } 
+else { errors += "4. Heart chambers\n"; }
+
+let q5 = prompt("5. Which animal is the only flying mammal? \nA) Flying squirrel; B) Eagle; C) Hummingbird; D) Bat.");
+if (q5 == "D") { score++; } 
+else { errors += "5. Only flying mammal\n"; }
+
+let q6 = prompt("6. What is a baby kangaroo called? \nA) Calf; B) Joey; C) Puppy; D) Kitten.");
+if (q6 == "B") { score++; } 
+else { errors += "6. Baby kangaroo\n"; }
+
+let q7 = prompt("7. Which ocean dweller is the largest on the planet? \nA) Sperm whale; B) Blue whale; C) Great white shark; D) Orca.");
+if (q7 == "B") { score++; } 
+else { errors += "7. Largest ocean dweller\n"; }
+
+let q8 = prompt("8. Which animal has three hearts? \nA) Starfish; B) Crab; C) Octopus; D) Jellyfish.");
+if (q8 == "C") { score++; } 
+else { errors += "8. Animal with three hearts\n"; }
+
+let q9 = prompt("9. To which family does the hyena actually belong? \nA) Felidae; B) Canidae; C) Hyaenidae; D) Ursidae.");
+if (q9 == "C") { score++; } 
+else { errors += "9. Hyena family\n"; }
+
+let q10 = prompt("10. Which fish can move on land? \nA) Goldfish; B) Shark; C) Anabas; D) Salmon.");
+if (q10 == "C") { score++; } 
+else { errors += "10. Fish on land\n"; }
+
+alert("Quiz Finished! \nYour score: " + score + "/10 \n\nErrors in: \n" + errors);
+
+
+
+
+
+
+
+
+/*
+var
+let
+const
+
+*/
+/*
+if(true){
+    var value1 = "value1";
+    let value2 = "value2";
+    const value3 = "value3";
+}
+
+console.log(value1);
+console.log(value2);
+console.log(value3);
+
+*/
+
+/*
+Примітиви
+Number: 10, 1, 2.5, 5.5, Infinity, NaN
+BigInt: 2^53, let bigNumber = 314314124124141241241241241n
+String: "Hello, world" = 'Hello, world' = `Hello, world`
+`` - цей тип лапок використовується щоб підставляти значення змінних. Інтерполяція: 
+let value = 'Text'
+console.log(`${Text}`)
+Boolean: true/false
+null: nothing
+undefined: задається автоматично JS, якщо змінна була створена без значення
+
+Symbol: ідентифікатор, який є унікальним та незмінним.
+
+Refference/скалдний тип
+Object
+let user = {
+    name: "John",
+    age: 30
+}
+
+typeof - оператор який дає можливість визначити тип
+*/
+
+
+/*
+Запросіть у користувача його ім'я та виведіть у відповідь «Привіт, (його ім'я)!».
+
+Запросіть у користувача його рік народження, підрахуйте скільки йому років і виведіть результат. Поточний рік вкажіть у коді як константу.
+
+Запросіть у користувача довжину сторони квадрата та виведіть його периметр.
+
+Запросіть у користувача радіус кола та виведіть його площу.
+
+Запросіть у користувача відстань у кілометрах між двома містами та за скільки годин він хоче дістатися. Порахуйте швидкість, з якою потрібно рухатися, щоб встигнути вчасно.
+
+Реалізуйте конвертор валют. Користувач вводить долари, програма конвертує у євро. Курс валюти зберігайте у константі.
+
+Користувач вказує обсяг флешки у ГБ. Програма має підрахувати скільки файлів розміром 820 МБ вміститься на флешку.
+
+Користувач вводить суму грошей у гаманці та вартість однієї шоколадки. Програма виводить скільки шоколадок може купити користувач і скільки здачі в нього залишиться.
+
+Запросіть у користувача тризначне число і виведіть його паліндром (число-перевертень).
+
+Запросіть у користувача ціле число і виведіть у відповідь парне або непарне. В завданні використовуйте логічні оператори і не використовуйте if та switch.
+*/
